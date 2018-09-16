ea9fe60bb0d4d6dffcd07cc31d35faeecf6624cd
Jaydev Thomke <jaydevthomke.in@gmail.com>
2018-09-16 12:51:58 -0700

    asdsad

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -7,8 +7,8 @@ import { Storage } from '@ionic/storage';
  */
 @Injectable()
 export class Api {
-  // url: string = 'https://pubg-mobile-api.herokuapp.com';
-  url: string = 'http://localhost:5000';
+  url: string = 'https://pubg-mobile-api.herokuapp.com';
+  // url: string = 'http://localhost:5000';
   token: string;
   constructor(public http: HttpClient, public storage: Storage) {
     this.setAPIHeaders();
